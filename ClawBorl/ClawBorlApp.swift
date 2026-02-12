be0a8a2a8f94dd0d940b85a50bebdcdcc8952079
garylauchina <garylauchina@gmail.com>
2026-02-12 13:49:08 +0800

    Initial Commit

---
+++ ClawBorl/ClawBorlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClawBorlApp.swift
+//  ClawBorl
+//
+//  Created by Gang Liu on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ClawBorlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
